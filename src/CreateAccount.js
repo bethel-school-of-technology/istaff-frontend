313fd5db271cd8f59ccce2f1f211ffe4fb20734f
Will Stafford <contact@willstafford.net>
2020-02-15 23:43:49 -0600

    Created Home & CreateAccount pages

---
+++ src/CreateAccount.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function CreateAccount() {
+    return (
+        <div>
+            <h1>Create Employee Account</h1>
+            <form>
+               <label>First Name:</label>
+               <input /><br></br>
+               <label>Last Name:</label>
+               <input /><br></br>
+               <label>Middle Initial:</label>
+               <input /><br></br>
+               <label>Date Of Birth:</label>
+               <input /><br></br>
+               <label>Hire Date:</label>
+               <input /><br></br>
+               <label>Username:</label>
+               <input /><br></br>
+               <button>Submit</button>
+            </form>
+        </div>
+                        )
+                    }
+                    
+export default CreateAccount;
