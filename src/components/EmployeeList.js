d1ce497121ef7153970bbd792f6acb357bab8128
Will Stafford <contact@willstafford.net>
2020-03-15 00:52:01 -0500

    Added code from frontend merge

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,45 +1,55 @@
-import React, { Component/*, useImperativeHandle*/ } from 'react';
+import React, { Component } from 'react';
 import axios from "axios";
 
 export default class EmployeeList extends Component {
     constructor(props) {
         super(props);
 
-        console.log('Employee List is active')
+        console.log('Employee List is active');
 
         this.state = {
             users: []
-        }
+        };
+
+        this.handleDelete = this.handleDelete.bind(this);
     }
 
     componentDidMount() {
-        axios.get('http://localhost:3001/users') 
+        axios.get('http://localhost:3001/users')
             .then(res => {
-                //console.log(res.data)
-                const users = res.data.map(obj => ({ firstName: obj.firstName, lastName: obj.lastName }));
+                console.log(res.data)
+                const users = res.data.map(obj => ({ idemp: obj.idemp, firstName: obj.firstName, lastName: obj.lastName }));
                 this.setState({ users });
-                //console.log(users);
+                console.log(users);
             });
     }
+
+    handleDelete = (idemp) => {
+        axios.delete(`http://localhost:3001/users/` + idemp)
+            .then(res => {
+                console.log(res)
+                console.log('it works')
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    };
 
     render() {
         return (
             <div>
-            <h1>Employee List</h1>
-            <ul>
-                {this.state.users.map(function (user, index) {
-                    return (
-                        <div key={index}>
-                            <h3>{user.firstName} {user.lastName}</h3>
-                        </div>
-                    )
-                }
-                )}
-            </ul>
+                <h1>Employee List</h1>
+                <ul>
+                    {this.state.users.map((user, index) => {
+                        return (
+                            <div key={index}>
+                                <h3>{user.firstName} {user.lastName}</h3>
+                                <button>Edit</button><button onClick={() => this.handleDelete(user.idemp)}>Delete</button>
+                            </div>
+                        )
+                    })}
+                </ul>
             </div>
         );
-
     }
-
 }
-//export default EmployeeList;
